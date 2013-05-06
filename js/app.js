05f7a60c994cd18bf594e45098fdcd49125bf1b9
Jurgen Van de Moere <jurgen@webberid.com>
2013-05-06 06:48:57 +0200

    Added concatenate filter

--- js/app.js
+++ js/app.js
@@ -3,3 +3,23 @@ var app = angular.module('boilerplate', []);
 app.controller('FirstController', function($scope) {
     $scope.message = 'Hello world';
 });
+
+/**
+ * Concatenate filter
+ *
+ * Usage: {{ input | concatenate:'one':'two' }}
+ */
+app.filter('concatenate', function(){
+    return function(input){
+
+        // Define delimiter
+        var delimiter = ' ';
+
+        // Skip index 0 because that is input
+        for(var i =1; i < arguments.length; i++){
+            input += delimiter + arguments[i];
+        }
+
+        return input;
+    }
+});
